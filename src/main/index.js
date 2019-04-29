074890a6265f7b5213c88614c2b55fdc66eea85c
John Lee <johnteee@gmail.com>
2019-04-29 15:43:14 +0800

    Make it closed when all window closed.

--- src/main/index.js
+++ src/main/index.js
@@ -35,9 +35,10 @@ function createWindow () {
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  // if (process.platform !== 'darwin') {
+  //   app.quit()
+  // }
+  app.quit()
 })
 
 app.on('activate', () => {
